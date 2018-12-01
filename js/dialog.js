744457c89eeeec1e6670788c920cadcfce0d7f86
Николай <arcmag-work@mail.ru>
2018-12-01 20:05:40 +0700

    Убирает немного лишнего кода

--- js/dialog.js
+++ js/dialog.js
@@ -4,30 +4,23 @@
   var setupBlock = document.querySelector('.setup');
   var setupBlockUpload = document.querySelector('.upload');
 
-  var mouseData = false;
+  var mouseData = {};
   setupBlockUpload.addEventListener('mousedown', function (e) {
     var dataElement = setupBlock.getBoundingClientRect();
 
-    mouseData = {
-      x: e.clientX - dataElement.left,
-      y: e.clientY - dataElement.top
-    };
+    mouseData.x = e.clientX - dataElement.left;
+    mouseData.y = e.clientY - dataElement.top;
 
     document.addEventListener('mousemove', mouseMove);
     document.addEventListener('mouseup', mouseUp);
   });
 
   var mouseMove = function (e) {
-    if (!mouseData) {
-      return;
-    }
-
-    setupBlock.style.left = (e.clientX - mouseData.x) + 'px';
-    setupBlock.style.top = (e.clientY - mouseData.y) + 'px';
+    setupBlock.style.left = e.clientX - mouseData.x + 'px';
+    setupBlock.style.top = e.clientY - mouseData.y + 'px';
   };
-  var mouseUp = function () {
-    mouseData = false;
 
+  var mouseUp = function () {
     document.removeEventListener('mousemove', mouseMove);
     document.removeEventListener('mouseup', mouseUp);
   };
