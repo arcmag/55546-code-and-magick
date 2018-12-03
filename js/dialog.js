e788854f236bf0121fc209e66ebee7e0565f6eb4
Николай <arcmag-work@mail.ru>
2018-12-03 17:12:13 +0700

    Корректирует формулу расчета позиции при перетаскивании окна персонажа

--- js/dialog.js
+++ js/dialog.js
@@ -1,12 +1,15 @@
 'use strict';
 
 (function () {
-  var setupBlock = document.querySelector('.setup');
+  var setupBlock = window.main.setupBlock;
   var setupBlockUpload = document.querySelector('.upload');
 
+  var dragged = false;
   var mouseData = {};
   setupBlockUpload.addEventListener('mousedown', function (e) {
     var dataElement = setupBlock.getBoundingClientRect();
+
+    dragged = false;
 
     mouseData.x = e.clientX - dataElement.left;
     mouseData.y = e.clientY - dataElement.top;
@@ -16,12 +19,23 @@
   });
 
   var mouseMove = function (e) {
-    setupBlock.style.left = e.clientX - mouseData.x + 'px';
+    dragged = true;
+
+    setupBlock.style.left = (e.clientX - mouseData.x + (setupBlock.offsetWidth / 2)) + 'px';
     setupBlock.style.top = e.clientY - mouseData.y + 'px';
   };
 
-  var mouseUp = function () {
+  var mouseUp = function (e) {
+    e.preventDefault();
     document.removeEventListener('mousemove', mouseMove);
     document.removeEventListener('mouseup', mouseUp);
+
+    if (dragged) {
+      var onClickPreventDefault = function (evt) {
+        evt.preventDefault();
+        setupBlockUpload.removeEventListener('click', onClickPreventDefault);
+      };
+      setupBlockUpload.addEventListener('click', onClickPreventDefault);
+    }
   };
 }());
---
+++ js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+(function () {
+  window.main = {
+    setupBlock: document.querySelector('.setup')
+  };
+}());
